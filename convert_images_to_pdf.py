7c047c025ef7ace13c1bcc4cdf384428db9496b3
csnick93 <nickvonroden@gmail.com>
2020-03-31 18:40:37 +0000

    small updates

--- convert_images_to_pdf.py
+++ convert_images_to_pdf.py
@@ -1,20 +1,30 @@
 import os
 from PIL import Image
+from pathlib import Path
 from PyPDF2 import PdfFileMerger
 
-input_path = '/home/nick/Downloads'
-im_files = [os.path.join(input_path, 's_1.jpeg'),
-        os.path.join(input_path, 's_2.jpeg'),
-        os.path.join(input_path, 's_3.jpeg')]
-out_file = os.path.expanduser('~/Schadensfall.pdf')
+input_path = '.'
+im_files = sorted(
+    [os.path.join(input_path, f) for f in os.listdir(input_path) if Path(f).suffix in ['.jpg', '.png', '.jpeg', '.pdf']])
+out_file = os.path.join(input_path, 'FehlendeSteuerUnterlagen.pdf')
 out_dir = os.path.dirname(out_file)
 
 tmp_pdfs = []
 
+rotate = False
+
+def rotate_im(im):
+    return im.rotate(90)
+
 # convert images to pdf
 for im_file in im_files:
+    if Path(im_file).suffix == '.pdf':
+        tmp_pdfs.append(im_file)
+        continue
     im = Image.open(im_file)
     im = im.convert('RGB')
+    if rotate:
+        im = rotate_im(im)
     file_ending = im_file.split('.')[-1]
     im_pdf_outfile = im_file.replace(file_ending, 'pdf')
     im.save(im_pdf_outfile)
@@ -29,5 +39,5 @@ for pdf in tmp_pdfs:
 merger.write(out_file)
 merger.close()
 
-for pdf in tmp_pdfs:
-    os.remove(pdf)
+# for pdf in tmp_pdfs:
+#     os.remove(pdf)
